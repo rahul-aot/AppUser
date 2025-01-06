0aab2cf698ad4dae111430a649ea37dbe6b65680
rahulap-aot <rahul.ap@aot-technologies.com>
2025-01-06 02:38:38 -0800

    basic login and logout

--- src/app/components/layout/layout.component.ts
+++ src/app/components/layout/layout.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-layout',
-  imports: [],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss'
 })
 export class LayoutComponent {
 
+  router = inject(Router)
+
+  onLogout(){
+    console.log('logout');
+    localStorage.removeItem('Angular19user');
+    this.router.navigateByUrl("login");
+  }
 }
